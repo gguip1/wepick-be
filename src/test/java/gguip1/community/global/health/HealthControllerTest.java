b84d2a734b637d250d709f01d0090c786284387f
gguip1 <gguip7554@naver.com>
2025-12-07 11:23:58 +0900

    test: authExcludedPath 변경 -> 테스트 관련 코드 수정

--- src/test/java/gguip1/community/global/health/HealthControllerTest.java
+++ src/test/java/gguip1/community/global/health/HealthControllerTest.java
@@ -1,10 +1,13 @@
 package gguip1.community.global.health;
 
+import gguip1.community.global.properties.AuthProperties;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles; // <-- import 추가
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -13,6 +16,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(controllers = HealthController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ActiveProfiles("dev-local")
+@Import(AuthProperties.class)
+@TestPropertySource(properties = {
+        "auth.excluded-paths=/health"
+})
 class HealthControllerTest {
 
     @Autowired
