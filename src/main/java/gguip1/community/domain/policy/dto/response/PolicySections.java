477d35827d81f7f059e3cbadbb69d768b572c065
gguip1 <gguip7554@naver.com>
2025-12-07 10:55:47 +0900

    feat: policy -> thymeleaf ssr -> csr 클라이언트

--- src/main/java/gguip1/community/domain/policy/dto/response/PolicySections.java
+++
@@ -1,9 +0,0 @@
-package gguip1.community.domain.policy.dto.response;
-
-import lombok.Builder;
-
-import java.util.List;
-
-@Builder
-public record PolicySections(String title, List<String> contents) {
-}
