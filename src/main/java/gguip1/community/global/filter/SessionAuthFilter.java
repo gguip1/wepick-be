16dfb2d2641657d3311e6ff8e2bd49352155719a
gguip1 <gguip7554@naver.com>
2025-12-07 10:42:08 +0900

    refactor: Error Exception 정리

--- src/main/java/gguip1/community/global/filter/SessionAuthFilter.java
+++ src/main/java/gguip1/community/global/filter/SessionAuthFilter.java
@@ -3,42 +3,37 @@ package gguip1.community.global.filter;
 import gguip1.community.global.context.SecurityContext;
 import gguip1.community.global.exception.ErrorCode;
 import gguip1.community.global.exception.ErrorException;
+import gguip1.community.global.properties.AuthProperties;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class SessionAuthFilter extends OncePerRequestFilter {
     private final HandlerExceptionResolver handlerExceptionResolver;
-    private final String[] excludedPaths;
+    private final AuthProperties authProperties;
     private final AntPathMatcher matcher = new AntPathMatcher();
-
-    public SessionAuthFilter(
-            HandlerExceptionResolver handlerExceptionResolver,
-            @Value("#{'${auth.excluded-paths}'.split(',')}") String[] excludedPaths
-    ) {
-        this.handlerExceptionResolver = handlerExceptionResolver;
-        this.excludedPaths = excludedPaths;
-    }
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
         String path = request.getRequestURI();
-        return Arrays.stream(excludedPaths)
-                .anyMatch(pattern -> matcher.match(pattern.trim(), path));
+
+        return authProperties.getExcludedPaths().stream()
+                .map(String::trim)
+                .anyMatch(pattern -> matcher.match(pattern, path));
     }
 
     @Override
